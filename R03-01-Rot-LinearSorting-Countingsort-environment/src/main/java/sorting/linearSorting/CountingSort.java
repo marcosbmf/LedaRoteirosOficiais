99cbc477239ee0b7ef71a2de3e1e2658c84a4934
marcosbmf <marcos.barros.filho@ccc.ufcg.edu.br>
2018-05-10 14:34:52 -0300

    Roteiro finalizado.

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -2,18 +2,91 @@ package sorting.linearSorting;
 
 import sorting.AbstractSorting;
 
-/**
- * Classe que implementa a estratégia de Counting Sort vista em sala. Procure
- * evitar desperdicio de memoria alocando o array de contadores com o tamanho
- * sendo o máximo inteiro presente no array a ser ordenado.
- * 
- */
 public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		
+		try {
+			this.nullArrayChecker(array);
+			this.rangeCheck(array, leftIndex, rightIndex);
+			this.nullElementCheck(array, leftIndex, rightIndex);
+		} catch (Exception e) {
+			return;
+		}
+		
+		if (array.length <= 1) {
+			return;
+		}
+		
+		int tamanhoArray = rightIndex - leftIndex +1;
+		int maior = getMaiorInteiro(array, leftIndex, rightIndex);
+		
+		Integer[] arrayResultado = new Integer[tamanhoArray];
+		int[] arraySomaAcumulada = new int[maior + 1];
+		
+		//Conta os números do array
+		int indice = 0;
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			indice = (int) array[i];
+			arraySomaAcumulada[indice] ++;
+		}
+		
+		//Soma acumulada
+		for (int i = 1; i < arraySomaAcumulada.length; i++) {
+			arraySomaAcumulada[i] += arraySomaAcumulada[i-1]; 
+		}
+		
+		//Coloca cada elemento em sua posição correta.
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			arraySomaAcumulada[(int) array[i]]--;
+			indice = arraySomaAcumulada[(int) array[i]];
+			arrayResultado[indice] = array[i]; 
+		}
+		
+		for (int i = 0; i < arrayResultado.length; i++) {
+			array[i+leftIndex] = arrayResultado[i];
+		}
+		
 	}
+
+	private int getMaiorInteiro(Integer[] array, int leftIndex, int rightIndex) {
+		int maior = array[leftIndex];
+		for(int i = leftIndex+1; i <= rightIndex; i++) {
+			if (array[i].compareTo(maior) > 0) {
+				maior = array[i];
+			}
+		}
+		return maior;
+	}
+	
+	private void rangeCheck(Object[] array, int leftIndex, int rightIndex) {
+		int length = array.length;
+		if (length == 0) {
+			if(leftIndex < 0 || (rightIndex != -1 && rightIndex != 0)){
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		}else if(leftIndex < 0 || rightIndex > length) {
+			throw new ArrayIndexOutOfBoundsException();
+		} else if (leftIndex > rightIndex) {
+			throw new UnsupportedOperationException();
+		}
+	}
+	
+	private void nullElementCheck(Object[] array, int leftIndex, int rightIndex) {
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			if (array[i] == null) {
+				throw new NullPointerException();
+			}
+		}
+	}
+	
+	private void nullArrayChecker(Object[] array) {
+		if (array == null) {
+			throw new NullPointerException();
+		}
+	}
+	
+	
 
 }
--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -12,8 +12,118 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		
+		try {
+			this.nullArrayChecker(array);
+			this.rangeCheck(array, leftIndex, rightIndex);
+			this.nullElementCheck(array, leftIndex, rightIndex);
+		} catch (Exception e) {
+			return;
+		}
+		
+		if (array.length <= 1) {
+			return;
+		}
+		
+		int tamanhoArray = rightIndex - leftIndex +1;
+		int maior = getMaiorInteiro(array, leftIndex, rightIndex);
+		int menor = getMenorInteiroNegativo(array, leftIndex, rightIndex);
+		
+		Integer[] arrayResultado = new Integer[tamanhoArray];
+		int[] arraySomaAcumuladaPositivo = new int[maior + 1];
+		int[] arraySomaAcumuladaNegativo = new int[(-1*menor) + 1];
+		
+		//Conta os números do array
+		int indice = 0;
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			indice = array[i];
+			if (indice < 0) {
+				arraySomaAcumuladaNegativo[-1*indice] ++;
+			}else {
+				arraySomaAcumuladaPositivo[indice]++;
+			}
+		}
+		
+		//Soma acumulada
+		for (int i = arraySomaAcumuladaNegativo.length-2; i >= 0; i--) {
+			arraySomaAcumuladaNegativo[i] += arraySomaAcumuladaNegativo[i+1]; 
+		}
+		
+		arraySomaAcumuladaPositivo[0] += arraySomaAcumuladaNegativo[0];
+		
+		for (int i = 1; i < arraySomaAcumuladaPositivo.length; i++) {
+			arraySomaAcumuladaPositivo[i] += arraySomaAcumuladaPositivo[i-1]; 
+		}
+		
+		//Coloca cada elemento em sua posição correta.
+		int elemento;
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			elemento = array[i];
+			if (elemento >= 0) {
+				arraySomaAcumuladaPositivo[elemento]--;
+				indice = arraySomaAcumuladaPositivo[elemento];
+				arrayResultado[indice] = elemento;
+			} else {
+				arraySomaAcumuladaNegativo[elemento*-1]--;
+				indice = arraySomaAcumuladaNegativo[elemento*-1];
+				arrayResultado[indice] = elemento;
+			}
+		}
+		
+		for (int i = 0; i < arrayResultado.length; i++) {
+			array[i+leftIndex] = arrayResultado[i];
+		}
+		
+	}
+
+	private int getMaiorInteiro(Integer[] array, int leftIndex, int rightIndex) {
+		Integer maior = array[leftIndex];
+		for(int i = leftIndex+1; i <= rightIndex; i++) {
+			if (array[i].compareTo(maior) > 0) {
+				maior = array[i];
+			}
+		}
+		if (maior < 0){
+			return 0;
+		}
+		return (int) maior;
+	}
+	
+	private int getMenorInteiroNegativo(Integer[] array, int leftIndex, int rightIndex) {
+		Integer menor = 0;
+		for(int i = leftIndex; i < rightIndex + 1; i++) {
+			if (array[i].compareTo(menor) < 0 ) {
+				menor = array[i];
+			}
+		}
+		return (int) menor;
+	}
+	
+	private void rangeCheck(Object[] array, int leftIndex, int rightIndex) {
+		int length = array.length;
+		if (length == 0) {
+			if(leftIndex < 0 || (rightIndex != -1 && rightIndex != 0)){
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		}else if(leftIndex < 0 || rightIndex > length) {
+			throw new ArrayIndexOutOfBoundsException();
+		} else if (leftIndex > rightIndex) {
+			throw new UnsupportedOperationException();
+		}
+	}
+	
+	private void nullElementCheck(Object[] array, int leftIndex, int rightIndex) {
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			if (array[i] == null) {
+				throw new NullPointerException();
+			}
+		}
+	}
+	
+	private void nullArrayChecker(Object[] array) {
+		if (array == null) {
+			throw new NullPointerException();
+		}
 	}
 
 }
