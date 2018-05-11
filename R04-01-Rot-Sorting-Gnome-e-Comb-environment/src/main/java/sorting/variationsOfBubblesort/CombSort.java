12877983f07e8ff8fdd9cb25d636818ce777ab89
marcosbmf <marcos.barros.filho@ccc.ufcg.edu.br>
2018-05-10 22:22:50 -0300

    R04 completo

--- R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/CombSort.java
+++ R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/CombSort.java
@@ -1,14 +1,93 @@
 package sorting.variationsOfBubblesort;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * The combsort algoritm.
  */
 public class CombSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		// Verificações iniciais
+		try {
+			this.nullArrayChecker(array);
+			this.rangeCheck(array, leftIndex, rightIndex);
+			this.nullElementCheck(array, leftIndex, rightIndex);
+		} catch (Exception e) {
+			return;
+		}
+
+		if (array.length <= 1) {
+			return;
+		}
+
+		// Gap inicial
+		int gap = (int) ((array.length) / 1.25);
+		if (gap == 0) {
+			gap = 1;
+		}
+
+		while (gap > 1) {
+			// Faz a comparação dos elementos utilizando o gap até quando possível.
+			for (int i = leftIndex; i < rightIndex; i++) {
+				if (i + gap <= rightIndex) {
+					if (array[i].compareTo(array[i + gap]) > 0) {
+						Util.swap(array, i, i + gap);
+					}
+				} else {
+					i = rightIndex + 1;
+				}
+			}
+
+			// Atualiza o gap.
+			gap = (int) (gap / 1.25);
+
+		}
+
+		this.bubbleSort(array, leftIndex, rightIndex);
+	}
+
+	private void bubbleSort(T[] array, int leftIndex, int rightIndex) {
+		boolean troca;
+		do {
+			troca = false;
+			for (int i = leftIndex; i < rightIndex; i++) {
+				if (array[i].compareTo(array[i + 1]) > 0) {
+					Util.swap(array, i, i + 1);
+					troca = true;
+				}
+			}
+		} while (troca);
+	}
+
+	// Métodos para verificações
+
+	private void rangeCheck(Object[] array, int leftIndex, int rightIndex) {
+		int length = array.length;
+		if (length == 0) {
+			if (leftIndex < 0 || (rightIndex != -1 && rightIndex != 0)) {
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		} else if (leftIndex < 0 || rightIndex > length) {
+			throw new ArrayIndexOutOfBoundsException();
+		} else if (leftIndex > rightIndex) {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	private void nullElementCheck(Object[] array, int leftIndex, int rightIndex) {
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			if (array[i] == null) {
+				throw new NullPointerException();
+			}
+		}
+	}
+
+	private void nullArrayChecker(Object[] array) {
+		if (array == null) {
+			throw new NullPointerException();
+		}
 	}
 }
--- R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/GnomeSort.java
+++ R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/GnomeSort.java
@@ -1,13 +1,76 @@
 package sorting.variationsOfBubblesort;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * The implementation of the algorithm must be in-place!
  */
 public class GnomeSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+
+		// Verificações iniciais
+		try {
+			this.nullArrayChecker(array);
+			this.rangeCheck(array, leftIndex, rightIndex);
+			this.nullElementCheck(array, leftIndex, rightIndex);
+		} catch (Exception e) {
+			return;
+		}
+
+		if (array.length <= 1) {
+			return;
+		}
+
+		// Posição inicial do gnomo.
+		int gnome = leftIndex;
+
+		while (gnome != rightIndex) {
+			/*
+			 * Gnomo compara o elemento em que ele está com o próximo caso haja necessidade
+			 * de realizar troca, ele volta.
+			 */
+			if (array[gnome].compareTo(array[gnome + 1]) > 0) {
+				Util.swap(array, gnome, gnome + 1);
+				gnome--;
+			} else {
+				gnome++;
+			}
+
+			if (gnome < 0) {
+				gnome = 0;
+			}
+		}
+
+	}
+
+	// Métodos para verificações
+
+	private void rangeCheck(Object[] array, int leftIndex, int rightIndex) {
+		int length = array.length;
+		if (length == 0) {
+			if (leftIndex < 0 || (rightIndex != -1 && rightIndex != 0)) {
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		} else if (leftIndex < 0 || rightIndex > length) {
+			throw new ArrayIndexOutOfBoundsException();
+		} else if (leftIndex > rightIndex) {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	private void nullElementCheck(Object[] array, int leftIndex, int rightIndex) {
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			if (array[i] == null) {
+				throw new NullPointerException();
+			}
+		}
+	}
+
+	private void nullArrayChecker(Object[] array) {
+		if (array == null) {
+			throw new NullPointerException();
+		}
 	}
 }
